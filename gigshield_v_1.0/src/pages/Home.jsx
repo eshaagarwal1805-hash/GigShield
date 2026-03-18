9497398ef61f2f0e6d710bbfc8234015850a157a
Sreshtha Mukherjee <mukherjeesreshtha503@gmail.com>
2026-03-18 15:06:33 +0530

    Initial React setup with Home page

---
+++ gigshield_v_1.0/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import "../styles/home.css";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>GigShield</h1>
+      <p>Your digital safety companion for gig workers.</p>
+
+      <div className="buttons">
+        <button>Start Shift</button>
+        <button>View Dashboard</button>
+      </div>
+    </div>
+  );
+}
