d163132bc4868d7cc0cc5b5cfe8e937b68e7c5e8
debasrita15 <cst.23bcta65@silicon.ac.in>
2026-03-31 19:53:42 +0530

    Revert "Login + Home progress""
    This reverts commit 68bdb31fa81a9c53ab45b74e5a2fed473080ecc1, reversing
    changes made to 91d1dfcd649fa864c7a625bbace1d2d7fda1e511.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* Catch-all → redirect to home */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+function App() {
+  return <Home />;
 }
+
+export default App;
--- frontend/src/pages/Dashboard.jsx
+++
@@ -1,5 +0,0 @@
-import GigShieldDashboard from "../components/GigshieldDashboard";
-
-export default function Dashboard() {
-  return <GigShieldDashboard />;
-}
